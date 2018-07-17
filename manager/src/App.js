8d18c1ae8d3fde2ebbe278e21053141af8ef9120
Vasko Kelkocev <pisimina@gmail.com>
2018-07-17 19:32:59 +0200

    firebase added

---
+++ manager/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyC_XFRkl0IWtcIx3etgrqLEi9L0pSO5G-A',
+            authDomain: 'manager-a1f10.firebaseapp.com',
+            databaseURL: 'https://manager-a1f10.firebaseio.com',
+            projectId: 'manager-a1f10',
+            storageBucket: 'manager-a1f10.appspot.com',
+            messagingSenderId: '842870707219'
+        };
+
+        firebase.initializeApp(config);
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
