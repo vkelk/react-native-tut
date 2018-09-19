4ec36412cca5bdb60a16dc537e7a2aee3a2c3cb1
Vasko Kelkocev <pisimina@gmail.com>
2018-09-19 20:36:21 +0200

    Error handling

--- CurrencyConverter/app/screens/Options.js
+++ CurrencyConverter/app/screens/Options.js
@@ -4,6 +4,7 @@ import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
@@ -12,6 +13,7 @@ const ICON_SIZE = 23;
 class Options extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
   }
 
   handleThemesPress = () => {
@@ -19,7 +21,9 @@ class Options extends Component {
   }
 
   handleSitePress = () => {
-    Linking.openURL('http://fixer.io').catch(() => alert('An error occured.'));
+    Linking.openURL('https://fixer.io').catch(() => 
+      this.props.alertWithType('error', 'Sorry!', 'Fixer.io cannot be open')
+    );
   }
   
   render() {
@@ -43,4 +47,4 @@ class Options extends Component {
   }
 }
 
-export default Options;
+export default connectAlert(Options);
